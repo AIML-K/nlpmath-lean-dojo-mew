ec9ce72b47af1071d6c8332cdfc166c2e7f43350
Kim, Wiback <kwb425@nonamefactory.ai>
2023-12-16 10:44:28 +0000

    emulating eval for demo

---
+++ MiniF2F/demo.lean
@@ -0,0 +1,9 @@
+import MiniF2F.Minif2fImport
+open BigOperators
+open Nat 
+open Real 
+open Rat
+
+theorem mathd_numbertheory_559 (b h v : ℝ) (h₀ : 0 < b ∧ 0 < h ∧ 0 < v) (h₁ : v = 1 / 3 * (b * h))
+(h₂ : b = 30) (h₃ : h = 13 / 2) : v = 65 := by sorry
+#align mathd_numbertheory_559 mathd_numbertheory_559
